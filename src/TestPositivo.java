8bbd856a942c4778bd04b7d511a915fffc063b81
pica.21257 <pica.alessandro.studente@itispaleocapa.it>
2025-02-03 13:49:58 +0100

    aggiunta catch nella classe TestPositivo

--- src/TestPositivo.java
+++ src/TestPositivo.java
@@ -4,7 +4,11 @@ public class TestPositivo {
             NumeroPositivo numeroPositivo = new NumeroPositivo(-5);
             System.out.println("Numero memorizzato: " + numeroPositivo.getNumero());
         } catch (NegativeException e) {
-            System.err.println("Eccezione catturata: " + e.getMessage());
+            System.out.println("Eccezione catturata: " + e.getMessage());
+        } catch (IntervalException e) {
+            System.out.println("Eccezione catturata: " + e.getMessage());
+        } catch (EsclusoException e) {
+            System.out.println("Eccezione catturata: " + e.getMessage());
         }
         System.out.println("Fine programma");
     }
